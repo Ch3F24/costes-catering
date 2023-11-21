9d13bc671bca1ed033949b6aa49a00fc8200c1b5
Chef <zsolt.horvath@wst.hu>
2023-11-21 10:01:31 +0100

    analytics

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -16,6 +16,8 @@
         @vite('resources/js/app.js')
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+        @include('site.partials.analytics')
+
     </head>
     <body>
         @yield('header')
